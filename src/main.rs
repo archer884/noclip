18230bed0ac9bb0a6cdfff819f3c3c52fa84e9b3
J/A <archer884@gmail.com>
2018-10-30 17:13:30 -0500

    First cut

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,60 @@
-use std::error::Error;
-use std::io::{self, Write};
 use clipboard::{ClipboardContext, ClipboardProvider};
+use std::env;
+use std::error::Error;
+use std::io::{self, Read, Write};
+
+enum Mode {
+    Read,
+    Write,
+}
+
+impl Mode {
+    fn from_args() -> Result<Mode, &'static str> {
+        let mut has_input = false;
+        let mut has_output = false;
+
+        for item in env::args().skip(1) {
+            match item.as_ref() {
+                "c" | "copy" => has_input = true,
+                "v" | "paste" => has_output = true,
+
+                _ => return Err("Invalid mode setting"),
+            }
+        }
+
+        match (has_input, has_output) {
+            (true, false) => Ok(Mode::Write),
+            (false, true) => Ok(Mode::Read),
+
+            _ => Err("Invalid mode setting"),
+        }
+    }
+}
 
 fn main() -> Result<(), Box<Error>> {
+    let mode = Mode::from_args()?;
+    
     let mut ctx: ClipboardContext = ClipboardProvider::new()?;
-
-    if let Ok(content) = ctx.get_contents() {
-        io::stdout().write(content.as_ref())?;
-        io::stdout().flush()?;
+    match mode {
+        Mode::Read => read(&mut ctx),
+        Mode::Write => write(&mut ctx),
     }
+}
+
+fn read(ctx: &mut ClipboardContext) -> Result<(), Box<Error>> {
+    let mut stdout = io::stdout();
+
+    stdout.write_all(ctx.get_contents()?.as_ref())?;
+    stdout.flush()?;
 
     Ok(())
+}
+
+fn write(ctx: &mut ClipboardContext) -> Result<(), Box<Error>> {
+    let mut content = String::new();
+
+    io::stdin().read_to_string(&mut content)?;
+    ctx.set_contents(content)?;
+
+    Ok(())    
 }
